f40d349a19a6572b33b8cc44cf9a469589a130fc
andgitisaac <andgitisaac@gmail.com>
2016-11-11 14:03:31 +0800

    modified array.h but with some deletion bugs

--- HW5/hw5/src/util/array.h
+++ HW5/hw5/src/util/array.h
@@ -35,42 +35,92 @@ public:
       ~iterator() {} // Should NOT delete _node
 
       // TODO: implement these overloaded operators
-      const T& operator * () const { return (*this); }
-      T& operator * () { return (*_node); }
-      iterator& operator ++ () { return (*this); }
-      iterator operator ++ (int) { return (*this); }
-      iterator& operator -- () { return (*this); }
-      iterator operator -- (int) { return (*this); }
-
-      iterator operator + (int i) const { return (*this); }
-      iterator& operator += (int i) { return (*this); }
-
-      iterator& operator = (const iterator& i) { return (*this); }
-
-      bool operator != (const iterator& i) const { return false; }
-      bool operator == (const iterator& i) const { return false; }
+      const T& operator * () const { return (*_node); }	// Done
+      T& operator * () { return (*_node); }	// Done
+      iterator& operator ++ () { ++_node; return (*this); } // Done
+      iterator operator ++ (int) { // Done
+      	iterator tmp = (*this);
+      	++(*this);
+      	// ++_node;
+      	return tmp;
+      }
+      iterator& operator -- () { --_node; return (*this); } // Done
+      iterator operator -- (int) { // Done
+      	iterator tmp = (*this);
+      	--(*this);
+      	// --_node;
+      	return tmp;
+      }
+      iterator operator + (int i) const { // Done 	
+      	iterator tmp = (*this);
+      	tmp += i;
+      	return tmp;
+      }
+      iterator& operator += (int i) { // Done
+      	_node += i;
+      	return (*this);
+      }
+      iterator& operator = (const iterator& i) { // Done
+      	_node = i._node;
+      	return (*this);
+      }
+      bool operator != (const iterator& i) const { // Done
+      	if(_node != i._node) return true;
+      	else return false;
+      }
+      bool operator == (const iterator& i) const { // Done
+      	return !(*this != i); // Implement by operator !=
+      }
 
    private:
       T*    _node;
    };
 
    // TODO: implement these functions
-   iterator begin() const { return 0; }
-   iterator end() const { return 0; }
-   bool empty() const { return false; }
-   size_t size() const { return 0; }
+   iterator begin() const { return iterator(_data); } // Done
+   iterator end() const { return iterator(_data + _size); } // Done
+   bool empty() const { return (!_size); } // Done
+   size_t size() const { return _size; } // Done
 
-   T& operator [] (size_t i) { return _data[0]; }
-   const T& operator [] (size_t i) const { return _data[0]; }
+   T& operator [] (size_t i) { return _data[i]; } // Done
+   const T& operator [] (size_t i) const { return _data[i]; } // Done
 
-   void push_back(const T& x) { }
-   void pop_front() { }
-   void pop_back() { }
+   void push_back(const T& x) { // Done
+   		if(_size == _capacity) expand();
+   		_data[_size] = x;
+   		++_size;
+   		// sort();
+   }
+   void pop_front() {
+   		if(_size == 1) --_size;
+   		if(_size >= 2){
+   			_data[0] = _data[_size - 1];
+   			--_size;
+   		}
+   		// erase(begin());
+   }
+   void pop_back() {  if(_size) --_size; } // Done
 
-   bool erase(iterator pos) { return false; }
-   bool erase(const T& x) { return false; }
+   bool erase(iterator pos) { // Done
+   		if(!_size) return false;
+   		if(pos == _data) pop_front();
+   		else{
+   			for(T* it = pos._node; it < _data + _size - 1; ++it)
+   				*(it) = *(it + 1);
+   			--_size;
+   		}   			
+   		return true;
+   }
+   bool erase(const T& x) { // Done	
+   		for(T* it = _data; it < _data + _size; ++it)
+   			if(*(it) == x){
+   				erase(iterator(it));
+   				return true;
+   			}
+   		return false;
+   }
 
-   void clear() { }
+   void clear() { _size = 0; } // Done
 
    // This is done. DO NOT change this one.
    void sort() const { if (!empty()) ::sort(_data, _data+_size); }
@@ -86,6 +136,15 @@ private:
    mutable bool  _isSorted;   // (optionally) to indicate the array is sorted
 
    // [OPTIONAL TODO] Helper functions; called by public member functions
+   void expand(){
+   		if(!_capacity) ++ _capacity;
+   		else _capacity *= 2;
+   		T* tmp = _data;
+   		_data = new T[_capacity];
+   		for(size_t i = 0; i < _size; ++i)
+   			_data[i] = tmp[i];
+   		delete [] tmp;
+   }
 };
 
 #endif // ARRAY_H
