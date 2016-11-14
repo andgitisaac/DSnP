f78b10d94d5aa6b12816eb965b38b8d817801020
andgitisaac <andgitisaac@gmail.com>
2016-11-14 11:25:24 +0800

    fix bugs of bst.h in function erase(iterator pos) & insert(const T& x)

--- HW5/hw5/src/util/bst.h
+++ HW5/hw5/src/util/bst.h
@@ -14,14 +14,6 @@
 
 using namespace std;
 
-#define REPLACE(A,B) if(B) B->_parent = A->_parent;\
-  							if(A->_parent)\
-  							(A->_parent->_right == A) ? (A->_parent->_right = B) : (A->_parent->_left = B);\
-  							else _root = B;
-
-#define TOMAX(A)     while(A->_right) A = A->_right
-#define TOMIN(A)     while(A->_left) A = A->_left
-
 template <class T> class BSTree;
 
 // BSTreeNode is supposed to be a private class. User don't need to see it.
@@ -65,68 +57,67 @@ public:
       const T& operator * () const { return _node->_data; } // Done
       T& operator * () { return _node->_data; } // Done
 
-   		iterator& operator ++ () { // Done
-   			// What if we ++ from end()??
-   			// Move forward to next larger value.
-   			BSTreeNode<T>* p;
-   			if(_node->_right){
-   				_node = _node->_right;
-   				while(_node->_left) _node = _node->_left;
-   			}
-   			else{
-   				p = _node->_parent;
-   				while(p && _node == p->_right){
-   					_node = p;
-   					p = p->_parent;
-   				}
-   				// if we were previously at the right-most node in
-   				// the tree, nodePtr = _tail, and the iterator specifies
-   				// the end of the list
+   	iterator& operator ++ () { // Done
+   		// What if we ++ from end()??
+   		// Move forward to next larger value.
+   		BSTreeNode<T>* p;
+   		if(_node->_right){
+   			_node = _node->_right;
+   			while(_node->_left) _node = _node->_left;
+   		}
+   		else{
+   			p = _node->_parent;
+   			while(p && _node == p->_right){
    				_node = p;
+   				p = p->_parent;
    			}
-   			return (*this);
+   			// if we were previously at the right-most node in
+   			// the tree, nodePtr = _tail, and the iterator specifies
+   			// the end of the list
+   			_node = p;
    		}
-   		iterator& operator ++ (int) { // Done
-   			iterator tmp = iterator(*this);
-   			++(*this);
-   			return tmp;
-   		}
-   		iterator& operator -- () { // Done
-   			// Move backward to largest value < current value.
-   			BSTreeNode<T>* p;
-   			if(_node->_left){
-   				_node = _node->_left;
-   				while(_node->_right) _node = _node->_right;
-   			}
-   			else{
-   				p = _node->_parent;
-   				while(p && _node == p->_left){
-   					_node = p;
-   					p = p->_parent;
-   				}
-   				// if we were previously at the left-most node in
-   				// the tree, it seems like that I have to well define begin()...
-   				_node = p;
-   			}
-   			return (*this);
-   		}
-   		iterator operator -- (int) { // Done
-   			iterator tmp = (*this);
-            --(*this);
-            return tmp;
-   		}
-   		iterator& operator = (const iterator& i) { // Done
-   			_node = i._node;
-   			return (*this);
-   		}
-
-   		bool operator != (const iterator& i) const { // Done
-   			if(_node != i._node) return true;
-   			else return false;
-   		}
-   		bool operator == (const iterator& i) const { // Done
-   			return !(*this != i);
-   		}
+   		return (*this);
+   	}
+   	iterator& operator ++ (int) { // Done
+   		iterator tmp = iterator(*this);
+   		++(*this);
+   		return tmp;
+		}
+   	iterator& operator -- () { // Done
+         // Move backward to largest value < current value.
+			BSTreeNode<T>* p;
+   		if(_node->_left){
+   			_node = _node->_left;
+   			while(_node->_right) _node = _node->_right;
+			}
+  			else{
+  				p = _node->_parent;
+  				while(p && _node == p->_left){
+  					_node = p;
+  					p = p->_parent;
+  				}
+  				// if we were previously at the left-most node in
+  				// the tree, it seems like that I have to well define begin()...
+  				_node = p;
+  			}
+  			return (*this);
+  		}
+  		iterator operator -- (int) { // Done
+  			iterator tmp = (*this);
+           --(*this);
+           return tmp;
+  		}
+  		iterator& operator = (const iterator& i) { // Done
+  			_node = i._node;
+  			return (*this);
+  		}
+  		bool operator != (const iterator& i) const { // Done
+  			if(_node != i._node) return true;
+  			else return false;
+  		}
+  		bool operator == (const iterator& i) const { // Done
+  			return !(*this != i);
+  		}
 
    private:
    		BSTreeNode<T>* _node;
@@ -143,63 +134,36 @@ public:
    bool empty() { return !_size; } // Done
    size_t size() { return _size; } // Done
 
-   void insert(const T& x) {
-   		// BSTreeNode<T>* current = _root;
-   		// insert_recursive(x, current, false);
-   		// ++_size;
-   		BSTreeNode<T>* temp = _root;
-   		   	while(1) {
-   		   		if(temp == _tail) {
-   		   			BSTreeNode<T>* n = new BSTreeNode<T>(x, _tail->_parent, _tail);
-   		   			if(temp == _root) _root = n;
-   		   			else _tail->_parent->_right = n;
-   		   			_tail->_parent = n;
-   		   			break;
-   		   		}
-   		   		else if(x < temp->_data) {
-   		   			if(temp->_left) temp = temp->_left;
-   		   			else {
-   		   				BSTreeNode<T>* n = new BSTreeNode<T>(x, temp);
-   		   				temp->_left = n;
-   		   				break;
-   		   			}
-   		   		}
-   		   		else {
-   		   			if(temp->_right) temp = temp->_right;
-   		   			else {
-   		   				BSTreeNode<T>* n = new BSTreeNode<T>(x, temp);
-   		   				temp->_right = n;
-   		   				break;
-   		   			}
-   		   		}
-   		   	}
-   		   	++_size;
+   void insert(const T& x) { // Done
+   	  BSTreeNode<T>* current = _root;
+   	  insert_recursive(x, current, false, false);
+		  ++_size;
    }
-   void pop_front() { erase(begin()); }
-   void pop_back() { erase(--end()); }
+   void pop_front() { erase(begin()); } // Done
+   void pop_back() { erase(--end()); } // Done
 
-   bool erase(iterator pos) {
+   bool erase(iterator pos) { // Done
    		if(pos == end()) return false;
    		BSTreeNode<T>* deleteNode = pos._node;
-
    		if(deleteNode->_right && deleteNode->_right != _tail){
    			if(deleteNode->_left){
    				// BSTreeNode<T>* succ = successor(deleteNode);
    				// T copyData = succ->_data;
    				// deleteNode->_data = copyData;
    				// succ->_parent->_left = 0;
+       //         // if(succ->_right) succ->_parent->_left = succ->_right;
+       //         // else succ->_parent->_left = 0;
        //         succ->_left = deleteNode->_left;
        //         succ->_right = deleteNode->_right;
        //         succ->_parent = deleteNode->_parent;
-
-               T s = (++pos)._node->_data;
-               erase(pos);
-               deleteNode->_data = s;
-               ++_size;
+          T s = (++pos)._node->_data;
+          erase(pos);
+          deleteNode->_data = s;
    			}
    			else{
    				reconnect(deleteNode, deleteNode->_right);
-               delete deleteNode;
+          delete deleteNode;
+          --_size;
    			}
    		}
    		else if(deleteNode->_left){
@@ -210,17 +174,18 @@ public:
    				_tail->_parent = max;
    				max->_right = _tail;
    			}
-            delete deleteNode;
+        delete deleteNode;
+        --_size;
    		}
    		else{
    			(deleteNode->_right == _tail) ? (reconnect(deleteNode, _tail))
    										: (reconnect(deleteNode, deleteNode->_left));
-            delete deleteNode;
-   		}
-   		--_size;
+        delete deleteNode;
+        --_size;
+   		}   		
    		return true;
    }
-   bool erase(const T& x) {
+   bool erase(const T& x) { // Done
       BSTreeNode<T>* current = _root;
       while(true){
          if(current == 0) return false; // No match.
@@ -231,30 +196,31 @@ public:
       }
    }
    void clear(){ // Done
-   	if(!empty()){
-   		iterator b = begin(), e = end(), it = b;
-   		while(it != e){
-   			++it;
-   			erase(b);
-   			b = it;
-   		}
-   	}
-   	_size = 0;
+   	  if(!empty()){
+   		 iterator b = begin(), e = end(), it = b;
+   		 while(it != e){
+   			  ++it;
+   			  erase(b);
+   			  b = it;
+   		 }
+   	  } 
+   	  _size = 0;
    }
 
    void sort() {}
-   void print(){ if(!empty()) treePrint(_root, 0); }
+   void print(){ if(!empty()) _Print(_root, 0); }
 
 private:
 	BSTreeNode<T>* _root;
 	BSTreeNode<T>* _tail;
 	size_t _size;
 
-	void insert_recursive(const T& x, BSTreeNode<T>* current, bool direction) {
+	void insert_recursive(const T& x, BSTreeNode<T>* current, bool direction, bool flag) {
 		// direction "false": go to left child 
 		if(current == _tail){
-			BSTreeNode<T>* newNode = new BSTreeNode<T>(x, _tail->_parent, 0, _tail);
-			if(empty()){
+         // cout << "case A" << endl;
+			BSTreeNode<T>* newNode = new BSTreeNode<T>(x, _tail->_parent, _tail, 0);
+			if(current == _root){
 				_root = newNode;
 			}
 			else{
@@ -263,39 +229,45 @@ private:
 			_tail->_parent = newNode;
 			return;
 		}
-		if(!current){
-			BSTreeNode<T>* newNode = new BSTreeNode<T>(x, current->_parent, 0, 0);
-			if(direction)
-				current->_parent->_right = newNode;
-			else
-				current->_parent->_left = newNode;
+		if(flag){
+         BSTreeNode<T>* newNode = new BSTreeNode<T>(x, current);
+			if(direction) current->_right = newNode;
+			else current->_left = newNode;
+      flag = false;
 			return;
 		}
-		if(x < current->_data){
-			current = current->_left;
-			insert_recursive(x, current, false);
+		if(x <= current->_data){
+      if(!current->_left) flag = true;
+      else current = current->_left;
+			insert_recursive(x, current, false, flag);
 		}
 		else{
-			current = current->_right;
-			insert_recursive(x, current, true);
+      if(!current->_right) flag = true;
+      else current = current->_right;    
+			insert_recursive(x, current, true, flag);
 		}
 		return;
 	}
 
 	BSTreeNode<T>* min(BSTreeNode<T>* current) {
-		// if(!current->_left) return current;
-		// else return min(current->_left);
-      while(current->_left) current = current->_left;
-      return current; 
+		if(!current->_left) return current;
+		else return min(current->_left);
 	}
 
 	BSTreeNode<T>* successor(BSTreeNode<T>* current) {
 		if(current->_right && current->_right != _tail)
 			return min(current->_right);
 		else if(current->_right == _tail){
-         current = current->_left;
+         if(current->_left) current = current->_left;
+         else return current;
+         BSTreeNode<T>* returnNode = current;
          while(current->_right) current = current->_right;
-         return current;
+         current->_right = _tail;
+         _tail->_parent = current->_right;
+         return returnNode;
+         // current = current->_left;
+         // while(current->_right) current = current->_right;
+         // return current;
       }
 		else{
 			BSTreeNode<T>* p;
@@ -309,7 +281,7 @@ private:
 		return current;
 	}
 
-	void reconnect(BSTreeNode<T>* x, BSTreeNode<T>* y) {
+	void reconnect(BSTreeNode<T>* x, BSTreeNode<T>* y) { // Done
 		if(y)
 			y->_parent = x->_parent;
 		if(x->_parent){
@@ -322,13 +294,13 @@ private:
 			_root = y;
 	}
 
-	void treePrint(BSTreeNode<T>* n, size_t l){
+	void _Print(BSTreeNode<T>* n, size_t l) { // Done
         for (size_t i = 0; i < l; ++i) cout << "  ";
 		if (n == 0 || n == _tail) cout << "[0]\n";
 		else {
 			cout << n->_data << endl;
-			treePrint(n->_left, l + 1);
-			treePrint(n->_right, l + 1);
+			_Print(n->_left, l + 1);
+			_Print(n->_right, l + 1);
 		}
    }
 
