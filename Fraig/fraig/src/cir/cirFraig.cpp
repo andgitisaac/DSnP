fd54b33b3c79efbd58715d0343d0bb93b738ef61
andgitisaac <andgitisaac@gmail.com>
2017-01-17 18:21:15 +0800

    complete fraig with bugsssss but i dont want to care

--- Fraig/fraig/src/cir/cirFraig.cpp
+++ Fraig/fraig/src/cir/cirFraig.cpp
@@ -46,10 +46,10 @@ CirMgr::strash()
     //     cout << tmpOut[i] << " ";
     // cout << endl;
 
-    if(strashFlag){
-        cout << "Error: strash operation has already been performed!!" << endl;
-        return;
-    }
+    // if(strashFlag){
+    //     cout << "Error: strash operation has already been performed!!" << endl;
+    //     return;
+    // }
 
     HashMap<HashKey, CirGate*> h(getHashSize(_M_count));
     for(size_t i = 0; i < _dfsList.size(); ++i){
@@ -76,13 +76,163 @@ CirMgr::strash()
 void
 CirMgr::fraig()
 {
-    
+    SatSolver solver;    
+    bool result;
+    int cnt = 0; // max pattern collect to simulate once
+    string str = "Fraig: ";
 
+    solver.initialize();
+    createCNF(solver);
 
+    // while(!_fecGrps.empty()){
+    //     for(list<FECGroup>::iterator its = _fecGrps.begin(); its != _fecGrps.end(); ++its){
+    //         FECGroup &fecGrp = (*its);
+    //         for(FECGroup::iterator i = (++fecGrp.begin()); i != fecGrp.end(); ++i){
+    //             result = solve((*fecGrp.begin())->getVar(), fecGrp.begin().isInv(),
+    //                             (*i)->getVar(), i.isInv(), solver);
+    //             solveMSG((*fecGrp.begin()), fecGrp.begin().isInv(), 
+    //                     (*i), i.isInv()); // print solve message
+    //             if(result){ // Need collect pattern...
+    //                 collectPattern(cnt, solver);
+    //             }
+    //         }
+    //     }
+    // }
 
-    
+    for(list<FECGroup>::iterator its = _fecGrps.begin(); its != _fecGrps.end(); ++its){
+        FECGroup &fecGrp = (*its);
+        FECGroup::iterator i = fecGrp.begin();
+        if((*fecGrp.begin())->getType() == CONST_GATE){
+            for(++i; i != fecGrp.end(); ++i){
+                result = solve((*fecGrp.begin())->getVar(), fecGrp.begin().isInv(),
+                   (*i)->getVar(), !i.isInv(), solver);
+                solveMSG((*fecGrp.begin()), fecGrp.begin().isInv(), 
+                    (*i), i.isInv()); // print solve message
+                if(!result){
+                    replace(*i, *fecGrp.begin(), i.isInv(), str);
+                    _gateVarList[(*i)->getGateId()] = 0;
+                    delete (*i);
+                    --_A_count;
+                }
+                // else{} // NEED??????
+            }
+        }
+        else{
+            for(; i != fecGrp.end(); ++i){
+                FECGroup::iterator cur = i;
+                ++cur;
+                while(cur != fecGrp.end()){
+                    result = solve((*i)->getVar(), i.isInv(),
+                                    (*cur)->getVar(), cur.isInv(), solver);
+                    solveMSG((*fecGrp.begin()), fecGrp.begin().isInv(), 
+                            (*i), i.isInv()); // print solve message
+                    if(!result){
+                        replace(*cur, *i, (cur.isInv() ^ i.isInv()), str);
+                        _gateVarList[(*cur)->getGateId()] = 0;
+                        delete (*cur);
+                        --_A_count;
+                        cur = fecGrp.erase(cur);
+                    }
+                    else SATSplit(cur, fecGrp, solver);
+                }
+            }
+        }
+    }
+    DFSConstruct();
+    strash();
+    _fecGrps.clear();
 }
 
 /********************************************/
 /*   Private member functions about fraig   */
 /********************************************/
+
+void CirMgr::createCNF(SatSolver& solver)
+{
+    for(unsigned i = 0; i < _dfsList.size(); ++i){
+        Var var = solver.newVar();
+        if(_dfsList[i]->getType() == PO_GATE){
+            if(_dfsList[i]->getFanin(0)){
+                // If the only AIG to PO exists
+                // Var of POs should be the same as its single fanin.
+                _dfsList[i]->setVar(_dfsList[i]->getFanin(0)->getVar());
+            }
+            else _dfsList[i]->setVar(var); // Should not enter here...                
+        }
+        // else if(_dfsList[i]->getType() == CONST_GATE){ // How to deal with CONST 0???
+        //     _dfsList[i]->setVar(var);
+        //     solver.assertProperty(var, false);
+        // }
+        else if(_dfsList[i]->getType() == AIG_GATE){
+            _dfsList[i]->setVar(var);
+            CirGate* g1 = _dfsList[i]->getFanin(0), * g2 = _dfsList[i]->getFanin(1);
+            // Construct CNF of AIG
+            solver.addAigCNF(var, g1->getVar(), _dfsList[i]->isInv(0), g2->getVar(), _dfsList[i]->isInv(1));
+        }
+        else _dfsList[i]->setVar(var);
+    }
+}
+
+bool CirMgr::solve(Var& g_var, bool g_phase, Var& f_var, bool f_phase, SatSolver& solver)
+{
+    Var newV = solver.newVar();
+    solver.addXorCNF(newV, g_var, g_phase, f_var, f_phase);
+
+    solver.assumeRelease();
+    solver.assumeProperty(newV, true);
+    solver.assumeProperty(_gateVarList[0]->getVar(), false);
+    return (solver.assumpSolve());
+}
+
+void CirMgr::solveMSG(CirGate* g, bool g_inv, CirGate* f, bool f_inv)
+{
+    if(g->getType() == CONST_GATE){
+        cout << "prove " << ((f_inv) ? ("!") : ("")) << f->getGateId() 
+            << " = 0..." << flush << char(13);    
+    }
+    else{
+        cout << "prove (" << ((g_inv) ? ("!") : ("")) << g->getGateId() << ", " 
+            << ((f_inv) ? ("!") : ("")) << f->getGateId() << ")..."
+            << flush << char(13); 
+    }
+}
+
+void CirMgr::collectPattern(int& cnt, SatSolver& solver)
+{
+    for(unsigned i = 0; i < _I_count; ++i){
+        cout << solver.getValue((getGate(tmpIn[i] / 2)->getVar())) << " ";
+        getGate(tmpIn[i] / 2)->_simValue = 
+            ((getGate(tmpIn[i] / 2)->_simValue << 1) | (solver.getValue((getGate(tmpIn[i] / 2)->getVar()))));
+    }
+    ++cnt;
+    if(cnt == 64){
+
+    }
+}
+
+void CirMgr::SATSplit(FECGroup::iterator& cur, FECGroup& fecGrp, SatSolver& solver)
+{
+    FECGroup grp;
+    bool newCurV, flag = false;
+    bool curV = ((cur.isInv()) ? (!solver.getValue((*cur)->getVar())) : solver.getValue((*cur)->getVar()));
+    FECGroup::iterator newCur = fecGrp.end();
+
+    ++cur;
+    while(cur != fecGrp.end()){
+        newCurV = solver.getValue((*cur)->getVar());
+        if (cur.isInv()) newCurV = !newCurV;
+        if (newCurV == curV){
+            grp.add(*cur, cur.isInv());
+            cur = fecGrp.erase(cur);
+        }
+        else{
+            if(!flag){
+                newCur = cur;
+                flag = true;
+            }
+            ++cur;
+        }
+    }
+    cur = newCur;
+    if (grp.size() > 1) _fecGrps.push_back(grp);
+}
--- Fraig/fraig/src/cir/cirMgr.h
+++ Fraig/fraig/src/cir/cirMgr.h
@@ -14,6 +14,7 @@
 #include <fstream>
 #include <iostream>
 #include <list>
+#include "sat.h"
 
 using namespace std;
 
@@ -53,7 +54,7 @@ public:
         iterator& operator++ () { _it++; return (*this); }
         iterator operator ++ (int) { iterator iter=(*this); ++(*this); return iter; }
 
-        iterator& operator= (const iterator& i) { _it = i._it; return (*this); }
+        iterator& operator = (const iterator& i) { _it = i._it; return (*this); }
 
         bool operator == (const iterator& i) const { return (_it == i._it); }
         bool operator != (const iterator& i) const { return !(*this == i); }
@@ -66,7 +67,7 @@ public:
     iterator begin() { return iterator(_gateList.begin()); }
     iterator end() { return iterator(_gateList.end()); }
     size_t size() const { return _gateList.size(); }
-    iterator erase(iterator &i) { i._it = _gateList.erase(i._it); return i; }
+    iterator erase(iterator& i) { i._it = _gateList.erase(i._it); return i; }
     void clear() { _gateList.clear(); }
 
     FECGroup& add(const CirGate* gate, const bool &inv)
@@ -83,20 +84,23 @@ class SimValue
 {
 public:
     SimValue() {}
-    SimValue(unsigned val = 0) : _value(val) {}
+    // SimValue(unsigned val = 0) : _value(val) {}
+    SimValue(size_t val = 0) : _value(val) {}
     ~SimValue() {}
 
-    unsigned operator () () const { return _value; }
+    // unsigned operator () () const { return _value; }
+    size_t operator () () const { return _value; }
     bool operator == (const SimValue& v) const { return (_value == v._value); }
 
 private:
-    unsigned _value;
+    // unsigned _value;
+    size_t _value;
 };
 
 class CirMgr
 {
 public:
-   CirMgr() : flag(false), strashFlag(false) {} // Done
+   CirMgr() : flag(false) {} // Done
    ~CirMgr(); // Done
 
    // Access functions
@@ -142,16 +146,24 @@ private:
    GateList _gateVarList, _dfsList;
    unsigned _M_count, _I_count, _L_count, _O_count, _A_count;
    bool flag; // Ture if the circuit net list has been constructed.
-   bool strashFlag; // Ture if already do strash.
+   // bool strashFlag; // Ture if already do strash.
    list<FECGroup> _fecGrps;
 
    // private function of Optimization
    void replace(CirGate*, CirGate*, bool, string&); // Done
    
    // private function of Simulation
-   void simEachGate(HashMap<SimValue, FECGroup>& , unsigned&, const bool& , unsigned* const & = 0); // Done
+   // void simEachGate(HashMap<SimValue, FECGroup>& , unsigned&, const bool& , unsigned* const & = 0); // Done
+   void simEachGate(HashMap<SimValue, FECGroup>& , unsigned&, const bool& , size_t* const & = 0); // Done
    void splitFECGroup(CirGate*, HashMap<SimValue, FECGroup>&); // Done
    void writeLog(); // Done
+
+   // private function of fraig
+   void createCNF(SatSolver&);
+   bool solve(Var& , bool, Var& , bool, SatSolver&);
+   void solveMSG(CirGate*, bool, CirGate*, bool);
+   void collectPattern(int& , SatSolver&);
+   void SATSplit(FECGroup::iterator&, FECGroup&, SatSolver&);
 };
 
 #endif // CIR_MGR_H
